05fa0ff64bc0ff279ba079d29792ecfaa514dfcd
Черепахин Василий <vasi.che@gmail.com>
2023-04-07 10:44:40 +0500

    Сравнивание чисел с заданной точностью

---
+++ src/test/java/ru/perm/v/hamcrest_demo/NumberTest.java
@@ -0,0 +1,17 @@
+package ru.perm.v.hamcrest_demo;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.closeTo;
+
+/**
+ * Сравнивание чисел с заданной точностью
+ */
+public class NumberTest {
+    @Test
+    public void givenDouble_whenCompare_thenCorrect() {
+        Double doubleVal = 7.70001;
+        // надо убедиться, что заданное число равно 7.7 с погрешностью 0.005
+        assertThat(doubleVal, closeTo(7.7, 0.005));
+    }}
