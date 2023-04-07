b0971c90db6b46adcd86ba9d482d9f1afee24c39
Черепахин Василий <vasi.che@gmail.com>
2023-04-07 10:48:51 +0500

    equalToIgnoringWhiteSpace

--- src/test/java/ru/perm/v/hamcrest_demo/StringTest.java
+++ src/test/java/ru/perm/v/hamcrest_demo/StringTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalToIgnoringCase;
+import static org.hamcrest.Matchers.equalToIgnoringWhiteSpace;
 
 public class StringTest {
     //  равна «petya», независимо от регистра?
@@ -13,5 +14,12 @@ public class StringTest {
         String b = "petya";
         // HAMCREST: equalToIgnoringCase
         assertThat(a, equalToIgnoringCase(b));
+    }
+
+    @Test
+    public void given2Strings_whenEqualIgnWhateSpace_thenCorrect() {
+        String a = "  Hi   Petya ";
+        // HAMCREST: equalToIgnoringWhiteSpace
+        assertThat(a, equalToIgnoringWhiteSpace("Hi Petya"));
     }
 }
