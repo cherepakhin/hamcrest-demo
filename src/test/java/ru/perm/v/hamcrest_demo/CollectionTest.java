f420c8ad952ea0a4cfce26845d09c76d65b681f4
Черепахин Василий <vasi.che@gmail.com>
2023-04-07 11:18:06 +0500

    hasItem()

--- src/test/java/ru/perm/v/hamcrest_demo/CollectionTest.java
+++ src/test/java/ru/perm/v/hamcrest_demo/CollectionTest.java
@@ -7,13 +7,47 @@ import java.util.List;
 
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.empty;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 
+/**
+ * Проверки List
+ */
 public class CollectionTest {
+    /**
+     * Не пустой
+     */
     @Test
     public void givenList_whenCheck_thenNotEmpty() {
         List<Integer> list = Arrays.asList(5, 2, 4);
         assertThat(list, is(not(empty())));
+    }
 
-    }}
+    /**
+     * Проверка размера
+     */
+    @Test
+    public void givenList_whenCheck_thenSize3() {
+        List<Integer> list = Arrays.asList(5, 2, 4);
+        assertThat(list, hasSize(3));
+    }
+
+    /**
+     * ВСЕ элементы  СПИСКА больше 0
+     */
+    @Test
+    public void givenList_whenCheck_thenPosotiveElements() {
+        List<Integer> list = Arrays.asList(5, 2, 4);
+        assertThat(list, everyItem(greaterThan(0)));
+
+    }
+
+    /**
+     * Список содержит элемент 5
+     */
+    @Test
+    public void givenList_whenCheck_thenHasItem() {
+        List<Integer> list = Arrays.asList(5, 2, 4);
+        assertThat(list, hasItem(5));
+
+    }
+}
